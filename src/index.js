64e1c1b8c2778e68163499926ae4b057976eb74b
yash chaudhary <yash240196@gmail.com>
2018-03-11 23:23:26 +0530

    React setup 80%

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("React Webpack Babel Setup");
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+
+ReactDOM.render(
+            <HelloWorld />, 
+            document.getElementById('app')
+            );
+
+module.hot.accept()
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,51 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  entry: ["./src/index.js"],
+  entry: ['react-hot-loader/patch', './src/index.js'],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      { 
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"]
+    extensions: ['*', '.js', '.jsx']
   },
   output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "bundle.js"
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: '[name].[hash].js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
   devServer: {
-    contentBase: "./dist"
+    contentBase: './dist',
+    hot: true
   }
 };
